0eb5ff6d1f8bccfa008e9efb02f73deead72cac0
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2026-04-26 00:10:10 +0530

    authentication alongwith creation of RFQ(auction) with a home page to display the listings of each category

--- cache.py
+++ cache.py
@@ -1,9 +1,13 @@
+import os
+
 import redis
+import dotenv
+dotenv.load_dotenv()
 
 cache = redis.Redis(
     host='redis-12395.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
     port=12395,
     decode_responses=True,
     username="default",
-    password="cGVKA8ohAJJ2uDQjNBGYBRElDsyaFLRC",
+    password=os.getenv("REDIS_PASSWORD"),
 )
