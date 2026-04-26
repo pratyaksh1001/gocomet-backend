fa10ad29bcfe1928d7367de85d99a080e778d65a
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2026-04-27 01:15:23 +0530

    postgres database error fixed

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
-from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, scoped_session
 from sqlalchemy import create_engine
 
 db_uri = "postgresql+psycopg2://qqqecejyqknddoebyvpa:mdxrifdespgvqjfekxnugzmqwhkyve@9qasp5v56q8ckkf5dc.leapcellpool.com:6438/kyfywbbcfwpfevczjkkh?sslmode=require"
 
 engine = create_engine(
     db_uri,
-    pool_pre_ping=True  # important for cloud DB
+    pool_pre_ping=True,
+    pool_recycle=300
 )
 
-session = Session(engine)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+session = scoped_session(SessionLocal)
